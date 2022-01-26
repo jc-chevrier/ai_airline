4f49e3962a183e139e89a174c57b8771ee2ce890
jc-chevrier <47176536+jc-chevrier@users.noreply.github.com>
2022-01-26 02:39:51 +0100

    Refactoring et amélioration, et correctifs.

--- ai_airline.bat
+++ ai_airline.bat
@@ -1,5 +1,3 @@
 @ECHO OFF
 
-::TODO vérifier existence et la créer si inexistante
-
 java -classpath bin fr.ul.miage.ai_airline.Main
--- ai_airline.sh
+++ ai_airline.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-#TODO vérifier existence et la créer si inexistante
-
 java -classpath bin fr.ul.miage.ai_airline.Main
